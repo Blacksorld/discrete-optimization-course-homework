38021b1e10cfa2890ea70ca83d98694721d54ede
randan <alagaster@yandex.ru>
2017-03-19 23:30:48 +0300

    fix

--- coding-hometask-1.hs
+++ coding-hometask-1.hs
@@ -1,6 +1,7 @@
 import Data.List ((\\))
 
 
+-- Решение задачи для произвольных весов
 canPack :: (Num a, Ord a) => [a] -> [a] -> [a] -> Bool
 canPack _ [] _ = True
 canPack [] _ []  = False
@@ -21,6 +22,7 @@ binarySearch f left right
 solveBpEvaluation :: [Rational] -> Maybe Int
 solveBpEvaluation weights = binarySearch (solveBpDecision weights) 0 (length weights)
 
+-- Заполнение одной корзины при оптимальном решении
 fillOneBin :: [Rational] -> [Rational] -> [Rational]
 fillOneBin bin [] = bin
 fillOneBin bin (w:ws)
